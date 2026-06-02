025fabe79cb9c77a9f039153a04523602939e3d9
Xavier Belanche <xbelanch@protonmail.com>
2026-06-02 18:58:06 +0200

    Load external media inline on demand

--- js/main.js
+++ js/main.js
@@ -172,13 +172,138 @@ $(function() {
 
 });
 
+$(function() {
+    function mediaProviderName(url, type) {
+        if (type === 'issuu') {
+            return 'Issuu';
+        }
+        if (/soundcloud\.com/.test(url)) {
+            return 'SoundCloud';
+        }
+        if (/youtube\.com|youtu\.be/.test(url)) {
+            return 'YouTube';
+        }
+        if (/vimeo\.com/.test(url)) {
+            return 'Vimeo';
+        }
+        if (/slides\.com/.test(url)) {
+            return 'Slides';
+        }
+        if (/ccma\.cat|tv3\.cat/.test(url)) {
+            return 'CCMA';
+        }
+        if (type === 'image') {
+            return 'Recurs visual';
+        }
+        return 'Contingut extern';
+    }
+
+    function mediaTypeFromUrl(url) {
+        if (/\.(jpe?g|png|gif|webp|bmp)(\?|#|$)/i.test(url)) {
+            return 'image';
+        }
+        return 'iframe';
+    }
+
+    function upgradeMediaPlaceholder($placeholder) {
+        var $link = $placeholder.find('a[href]').first();
+        var src = $placeholder.data('src') || ($link.length ? $link.attr('href') : '');
+        var type = $placeholder.data('media-type') || (src ? mediaTypeFromUrl(src) : '');
+        var title = $placeholder.data('media-title') || mediaProviderName(src, type);
+        var fallback = src;
+
+        if (!type && !$placeholder.data('configid')) {
+            return;
+        }
+
+        $placeholder
+            .addClass('media-placeholder')
+            .attr('data-media-type', type);
+
+        if (src) {
+            $placeholder.attr('data-src', src);
+        }
+
+        var actions = '<button type="button" class="media-placeholder__button js-media-load">Carrega el contingut aquí</button>';
+        if (fallback) {
+            actions += '<a class="media-placeholder__link" href="' + fallback + '" target="_blank" rel="noopener noreferrer">Obre en pestanya nova</a>';
+        }
+
+        $placeholder.html(
+            '<div class="media-placeholder__body">' +
+                '<h3 class="media-placeholder__title">' + title + '</h3>' +
+                '<p class="media-placeholder__text">Aquest contingut extern es carregarà només quan el demanis.</p>' +
+                actions +
+            '</div>'
+        );
+    }
+
+    function loadIssuu($placeholder) {
+        var configId = $placeholder.data('configid');
+        if (!configId) {
+            return;
+        }
+
+        $placeholder.addClass('is-loaded').html(
+            '<div data-configid="' + configId + '" style="width:100%; height:100%;" class="issuuembed"></div>'
+        );
+
+        var script = document.createElement('script');
+        script.async = true;
+        script.src = 'https://e.issuu.com/embed.js';
+        document.body.appendChild(script);
+    }
+
+    function loadInlineMedia($placeholder) {
+        if ($placeholder.hasClass('is-loaded')) {
+            return;
+        }
+
+        var type = $placeholder.attr('data-media-type');
+        var src = $placeholder.attr('data-src');
+
+        if (type === 'issuu') {
+            loadIssuu($placeholder);
+            return;
+        }
+
+        if (!src) {
+            return;
+        }
+
+        if (type === 'image') {
+            $placeholder.addClass('is-loaded').html(
+                '<img class="media-placeholder__image img-responsive img-thumbnail" src="' + src + '" alt="">'
+            );
+            return;
+        }
+
+        var allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+        $placeholder.addClass('is-loaded').html(
+            '<iframe class="media-placeholder__frame" src="' + src + '" allow="' + allow + '" allowfullscreen></iframe>'
+        );
+    }
+
+    $('.external-embed-placeholder, .media-placeholder').each(function() {
+        upgradeMediaPlaceholder($(this));
+    });
+
+    $(document).on('click', '.js-media-load', function(event) {
+        event.preventDefault();
+        loadInlineMedia($(this).closest('.media-placeholder'));
+    });
+});
+
 
     // by Marc Català! Thank you!
 
 $(document).ready(function() {
     var url = document.location.pathname;
-    var regnum = /\/(\d+)\/i/;
-    var num = url.match(regnum)[1];
+    var match = url.match(/\/(\d+)\//);
+    if (!match) {
+        return;
+    }
+    var num = match[1];
     var headerImg = 'background-header.opt.webp';
 
     if (num) {
